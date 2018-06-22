f1f09e3cbfb49f2004dd2c24d6ea5eda9f2998ba
JordanFicarra <jordanficarra2@gmail.com>
2018-06-22 13:10:24 +0000

    all

---
+++ projects/runtime/js/level01.js
@@ -0,0 +1,68 @@
+var level01 = function (window) {
+
+    window.opspark = window.opspark || {};
+
+    var draw = window.opspark.draw;
+    var createjs = window.createjs;
+
+    window.opspark.runLevelInGame = function(game) {
+        // some useful constants 
+        var groundY = game.groundY;
+
+        // this data will allow us to define all of the
+        // behavior of our game
+        var levelData = {
+            name: "Robot Romp",
+            number: 1, 
+            speed: -3,
+            gameItems: [
+                {type: 'sawblade',x:400,y:groundY},
+                {type: 'sawblade',x:600,y:groundY},
+                {type: 'sawblade',x:900,y:groundY}
+            ]
+        };
+        window.levelData = levelData;
+        // set this to true or false depending on if you want to see hitzones
+        game.setDebugMode(false);
+
+        // BEGIN EDITING YOUR CODE HERE
+        var hitZoneSize = 25;
+        var damageFromObstacle = 50;
+        var myObstacle = game.createObstacle(hitZoneSize,damageFromObstacle);
+        myObstacle.x = 400;
+        myObstacle.y = 490;
+        game.addGameItem(myObstacle);
+        var obstacleImage = draw.bitmap('img/sawblade.png');
+        myObstacle.addChild(obstacleImage);
+        obstacleImage.x = -25;
+        obstacleImage.y = -25;
+        var hitZoneSize = 25;
+        var damageFromObstacle = 50;
+        var myObstacle = game.createObstacle(hitZoneSize,damageFromObstacle);
+        myObstacle.x = 600;
+        myObstacle.y = 490;
+        game.addGameItem(myObstacle);
+        var obstacleImage = draw.bitmap('img/sawblade.png');
+        myObstacle.addChild(obstacleImage);
+        obstacleImage.x = -25;
+        obstacleImage.y = -25;
+        var hitZoneSize = 25;
+        var damageFromObstacle = 50;
+        var myObstacle = game.createObstacle(hitZoneSize,damageFromObstacle);
+        myObstacle.x = 850;
+        myObstacle.y = 380;
+        game.addGameItem(myObstacle);
+        var obstacleImage = draw.bitmap('img/sawblade.png');
+        myObstacle.addChild(obstacleImage);
+        obstacleImage.x = -25;
+        obstacleImage.y = -25;
+    };
+
+};
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports = level01;
+}
